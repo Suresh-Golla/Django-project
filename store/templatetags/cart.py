837a991f415e5dca23133b7d625cdcc9c1637b3f
Suresh-Golla <suresh.golla96@gmail.com>
2021-10-10 17:07:37 +0530

    add product to cart

--- store/templatetags/cart.py
+++ store/templatetags/cart.py
@@ -10,6 +10,14 @@ def is_in_cart(product, cart):
             return True
     return False;  
 
+@register.filter(name='cart_quantity')
+def cart_quantity(product, cart):
+    keys = cart.keys()
+    for id in keys:
+        if int(id) == product.id:
+            return cart.get(id)
+    return 0; 
+
 
     #     try:
     #         a = int(id)
